eaa470ee7d63ae71590b9b898dbe3b92b96dfa5d
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2020-03-19 12:24:54 -0700

    added testing files

---
+++ single_player/hand_test.py
@@ -0,0 +1,21 @@
+import unittest
+from single_player.round_functions.hand_functions import Hand
+
+
+class HandTest(unittest.TestCase):
+
+    def test_retrieve_pairs(self):
+        self.assertEqual(True, False)
+
+    def test_retrieve_tractors(self):
+        pass
+
+    def test_gt(self):
+        pass
+
+    def test_check_is_one_suit(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
